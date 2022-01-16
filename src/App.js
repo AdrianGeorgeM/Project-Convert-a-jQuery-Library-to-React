c375f457c27dc31a1e5294a920ce4dd4d7b35cb8
Adrian <adrian_george28@yahoo.com>
2022-01-16 10:58:38 +0000

    Create Modal and Input Field

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Pages/Home";
+import Employees from "./Pages/EmployeesPage";
+import React, { useReducer } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import { reducer, initialState } from "./states/reducers";
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    display: flex;
+    justify-content: center;
+    box-sizing: border-box;
+  }
+  code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+  }
+
+  th {
+    font-size: 14px;
+  }
+  td {
+    font-size: 15px;
+  }
+
+`;
+
+export const AppContext = React.createContext();
 function App() {
+  const contextValue = useReducer(reducer, initialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppContext.Provider value={contextValue}>
+        <GlobalStyle />
+        <Router basename={process.env.PUBLIC_URL}>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/employee-list'>
+              <Employees />
+            </Route>
+          </Switch>
+        </Router>
+      </AppContext.Provider>
+    </>
   );
 }
 
